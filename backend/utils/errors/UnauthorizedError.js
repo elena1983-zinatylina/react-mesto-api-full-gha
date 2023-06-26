15ff808589eac837e31c603a63e38c583b7e8163
zinatylina elena <tortdekor2016@yandex.ru>
2023-06-26 13:56:14 +0300

    dct eljkbnm

--- backend/utils/errors/UnauthorizedError.js
+++
@@ -1,8 +0,0 @@
-const { STATUS_CODES } = require('../constants');
-
-module.exports = class UnauthorizedError extends Error {
-  constructor(message) {
-    super(message);
-    this.statusCode = STATUS_CODES.UNAUTHORIZED;
-  }
-};
