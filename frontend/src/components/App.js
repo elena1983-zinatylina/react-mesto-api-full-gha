89b6c13b245e0e2dcd049ed30838273626e38ee3
zinatylina elena <tortdekor2016@yandex.ru>
2023-06-28 11:27:24 +0300

    some progress

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -212,8 +212,8 @@ function App() {
         }
       })
             .catch((err) => {
-                setInfoSuccess(false); // статус регистрации
-                setRegisterSuccess(true); //открываем попап
+                //setInfoSuccess(false); // статус регистрации
+               // setRegisterSuccess(true); //открываем попап
                 console.log(err);
             })
     };
