7b9a9640636c4b1459b761947932c4df6f80e260
zinatylina elena <tortdekor2016@yandex.ru>
2023-06-30 11:02:54 +0300

    deleted

--- frontend/src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-function Footer({ loggedIn }) {
-    return (
-        <footer className={loggedIn ? 'footer' : 'footer_invisible'}>
-            <p className="footer__copyright"> &#169; {new Date().getFullYear()} Зинатулина Елена</p>
-        </footer>
-    );
-}
-
-export default Footer;
